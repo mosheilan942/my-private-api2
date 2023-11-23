2b5c36d6642b0e2c2c2ff43d7ebb6689c2c0b361
orishu10 <orishu10@gmail.com>
2023-11-23 14:14:55 +0200

    add endpoint to prudects

--- client/src/api/productsAPI.ts
+++ client/src/api/productsAPI.ts
@@ -1,11 +1,13 @@
 import Product from "../types/Product";
 import handleApiRes from "./apiResHandler";
 
+
 //external
 async function getTop5Products(): Promise<Product[]> {
     const response = await fetch('/api/products/topFiveProducts');
   return await handleApiRes(response);
 }
+
 //external
 async function getProduct(pid:string): Promise<Product> {
     const response = await fetch(`/api/products/${pid}`);
@@ -13,6 +15,5 @@ async function getProduct(pid:string): Promise<Product> {
     
     return await handleApiRes(response);
 }
-
 
 export default { getTop5Products, getProduct }
