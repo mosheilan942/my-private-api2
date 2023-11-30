b86745f7dae1c5a8650b7b459453155b1d3d35a2
chaimKleinerman1 <chaimKleinerman1@mail.com>
2023-11-30 19:04:58 +0200

    chang cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,43 +1,44 @@
-import cors from 'cors';
-import dotenv from 'dotenv';
-import morgan from 'morgan';
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import { errorHandler, notFound } from './middlewares/errorsMiddleware.js';
-import userRoutes from './routes/userRoutes.js';
-import productRoutes from './routes/productsRoutes.js';
-import categoryRoutes from './routes/categoryRoutes.js';
-import cartRoutes from './routes/cartRoutes.js';
+import cors from "cors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import express from "express";
+import cookieParser from "cookie-parser";
+import { errorHandler, notFound } from "./middlewares/errorsMiddleware.js";
+import userRoutes from "./routes/userRoutes.js";
+import productRoutes from "./routes/productsRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import cartRoutes from "./routes/cartRoutes.js";
 import pg from "pg";
 const { Pool } = pg;
-import { config } from 'dotenv';
-import ordersRouter from './routes/ordersRouets.js';
-import bannerRoutes from './routes/bannerRouetes.js';
+import { config } from "dotenv";
+import ordersRouter from "./routes/ordersRouets.js";
+import bannerRoutes from "./routes/bannerRouetes.js";
 config();
 
 const app = express();
 
-
 // APP CONFIGS
 // console.log(process.env);
-app.use(cors({ origin: true, credentials: true }));
+app.use(
+    cors({
+        origin: "*",
+    })
+);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use('/api/users', userRoutes);
-app.use('/api/users', cartRoutes);
-app.use('/api/products', productRoutes);
-app.use('/api/orders', ordersRouter);
-app.use('/api/banner', bannerRoutes);
-app.use('/api', categoryRoutes);
-
+app.use("/api/users", userRoutes);
+app.use("/api/users", cartRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/orders", ordersRouter);
+app.use("/api/banner", bannerRoutes);
+app.use("/api", categoryRoutes);
 
 app.use(errorHandler);
 
-
 // app.post('/api/payment/check', (req, res) => {
-//   const debitCardDetails = req.body; 
+//   const debitCardDetails = req.body;
 //   setTimeout(() => {
 //     console.log(debitCardDetails);
 //     if (true) {
@@ -47,7 +48,6 @@ app.use(errorHandler);
 //     }
 //   }, 3000);
 // });
-
 
 // // Order.
 // app.post('/api/payment/order', (req, res) => {
@@ -59,18 +59,16 @@ app.use(errorHandler);
 //     } else {
 //       res.status(400).json({ message: 'An error occurred in the ordering process !!!' });
 //     }
-//   }, 3000); 
+//   }, 3000);
 // });
 
-
-
 const port = 5000;
-export const connectionString = process.env.CONNECTION_STRING
+export const connectionString = process.env.CONNECTION_STRING;
 //await connectDB();
 app.listen(port, async () => {
-  const pool = new Pool({connectionString: connectionString})
-  const res = await pool.connect()
-  res.release()
-  console.log(`Database connection test completed successfully`);
-  console.log(`\nServer is running at port ${port}...`);
+    const pool = new Pool({ connectionString: connectionString });
+    const res = await pool.connect();
+    res.release();
+    console.log(`Database connection test completed successfully`);
+    console.log(`\nServer is running at port ${port}...`);
 });
